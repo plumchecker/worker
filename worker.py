8da1bfab1484cfd5506c8566dff63483857f7f1d
Tenzy21 <32966784+Tenzy21@users.noreply.github.com>
2023-02-11 22:15:39 +0300

    Update worker.py to version 2
    Added:
    Checking via regular expression

--- worker.py
+++ worker.py
@@ -1,3 +1,4 @@
+import re
 import json
 
 def txt_to_json(file_name):
@@ -5,20 +6,20 @@ def txt_to_json(file_name):
 
     with open(file_name, 'r') as file:
         for line in file:
-            if ';' in line:
-                login, password = line.strip().split(';')
-            elif ':' in line:
-                login, password = line.strip().split(':')
+            match = re.search("^(.*)[;:]", line)
+            if match:
+                login = match.group(1)
+                password = line[match.end():].strip()
+                logins[login] = password
             else:
-                raise ValueError("Invalid separator in line: {}".format(line))
-            logins[login] = password
+                raise ValueError("Invalid line format: {}".format(line))
 
     with open(file_name + '.json', 'w') as file:
         json.dump(logins, file)
 
 txt_to_json("1.txt")
 
-#TODO: 
-# 1)check for separator in password
-# 2)check for separator in login // UPD: no ':' or ';' symbols in login
-# 3)add regular expressions
+#TODO:
+# 1)optimize with threads
+# 2)optimize with chunks (optional)
+# 3)check for another separators (optional)
