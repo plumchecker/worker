952edc6c622e104e1f2dbd8471c8a89fdbdd638a
Tenzy21 <32966784+Tenzy21@users.noreply.github.com>
2023-02-11 22:16:36 +0300

    Update worker.py to version 3
    Added:
    Optimization with threads

--- worker.py
+++ worker.py
@@ -1,18 +1,27 @@
+import concurrent.futures
 import re
 import json
+
+def process_line(line):
+    match = re.search("^(.*)[;:]", line)
+    if match:
+        login = match.group(1)
+        password = line[match.end():].strip()
+        return (login, password)
+    else:
+        raise ValueError("Invalid line format: {}".format(line))
 
 def txt_to_json(file_name):
     logins = {}
 
     with open(file_name, 'r') as file:
-        for line in file:
-            match = re.search("^(.*)[;:]", line)
-            if match:
-                login = match.group(1)
-                password = line[match.end():].strip()
-                logins[login] = password
-            else:
-                raise ValueError("Invalid line format: {}".format(line))
+        lines = file.readlines()
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        results = [executor.submit(process_line, line) for line in lines]
+        for future in concurrent.futures.as_completed(results):
+            login, password = future.result()
+            logins[login] = password
 
     with open(file_name + '.json', 'w') as file:
         json.dump(logins, file)
@@ -20,6 +29,5 @@ def txt_to_json(file_name):
 txt_to_json("1.txt")
 
 #TODO:
-# 1)optimize with threads
-# 2)optimize with chunks (optional)
-# 3)check for another separators (optional)
+# 1)optimize with chunks (optional)
+# 2)check for another separators (optional)
