46fba5c0dab10ed51b7e41945de1a58f0fce6625
Tenzy21 <32966784+Tenzy21@users.noreply.github.com>
2023-02-12 17:04:26 +0300

    Update worker.py
    Added:
    Splitting login data to email and domain
    Adding login/domain/password info into payload
    Sending requests with data to database
    
    Fixed:
    Fixed issue with chunk sizes and EOF
    Fixed issue with wrong file encoding
    Fixed some other bugs

--- worker.py
+++ worker.py
@@ -1,6 +1,14 @@
 import concurrent.futures
 import re
 import json
+import requests
+
+def send_request(data_payloads):
+    counter = 0
+    hosts = {}
+
+    resp = requests.post("http://25.68.246.17:30001/api/leaks",json={"leaks": data_payloads})
+    print(resp.json())
 
 def process_line(line):
     match = re.search("^(.*)[;:]", line)
@@ -12,30 +20,41 @@ def process_line(line):
         raise ValueError("Invalid line format: {}".format(line))
 
 def txt_to_json(file_name):
-    logins = {}
-    batch_size = 100000
+    payload = {}
+    payloads = []
+    batch_size = 100
+    last_iter = False
+    
+    with open(file_name, 'r', encoding="utf-8") as file:
+        lines = file.readlines()
+        file_size = len(lines)
 
-    with open(file_name, 'r') as file:
-        while True:
+    with open(file_name, 'r', encoding="utf-8") as file:
+        while not last_iter:
+            
             lines = [next(file).strip() for _ in range(batch_size)]
-            if not lines:
-                break
+            file_size = file_size - batch_size
+            
+            if batch_size > file_size:
+                batch_size = file_size
+
+            if file_size == 0:
+                last_iter = True
 
             with concurrent.futures.ThreadPoolExecutor() as executor:
                 results = [executor.submit(process_line, line) for line in lines]
                 for future in concurrent.futures.as_completed(results):
                     login, password = future.result()
-                    logins[login] = password
+                    email = login.split("@")[0]
+                    domain = login.split("@")[1]
+                    payload = {"email": email, "domain": domain, "password": password}
+                    payloads.append(payload)
+            
+            send_request(payloads)
+            #with open(file_name + '.json', 'w', encoding="utf-8") as file1:
+            #    json.dump(payload, file1)
 
-            with open(file_name + '.json', 'w') as file:
-                json.dump(logins, file)
-
-            logins = {}
+            payload = {}
+            payloads = []
 
 txt_to_json("1.txt")
-
-#TODO:
-# 1)check for another separators (optional)
-# 2)realize checker for EOF in while(true) cycle
-# 3)check for bugs
-# 4)add login/domain separation
